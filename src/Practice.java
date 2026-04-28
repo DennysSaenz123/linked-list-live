f6e047b94949a9d2644e0aeab51cf512a6d5aff6
DennysSaenz <SaenzSegovia.Dennys@student.greenriver.edu>
2026-04-28 11:45:23 -0700

    folllowed along in class

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,16 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node();
+        myNode.value = 'w';
+
+        Node otherNode = new Node();
+        otherNode.value = 'e';
+
+        Node coolNode = otherNode;
+        coolNode.value = 'x';
+
+        System.out.println(myNode.value);
+        System.out.println(otherNode.value);
+        System.out.println(coolNode.value);
     }
 }
