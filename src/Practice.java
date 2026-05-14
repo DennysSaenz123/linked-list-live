bbcaf002775038a9f4ff64cfcae33351b0063584
DennysSaenz <SaenzSegovia.Dennys@student.greenriver.edu>
2026-05-14 11:15:51 -0700

    followed in class livecode

--- src/Practice.java
+++ src/Practice.java
@@ -3,20 +3,57 @@ public class Practice {
         Node myNode = new Node('w', null);
         Node otherNode = new Node('x', null);
         Node coolNode = new Node('e', null);
+        Node helloNode = new Node('f', null);
 
-        Node currentNode = myNode;
+        // Node currentNode = myNode;
 
         myNode.next = otherNode;
         otherNode.next = coolNode;
+        coolNode.next = helloNode;
 
-        while(currentNode != null) {
-            System.out.println(currentNode.value);
-            currentNode = currentNode.next;
+        // int numNodes = length(myNode);
+        // System.out.println(numNodes);
+
+        Node newHead = removeAt(myNode, 2);
+
+        Node current = newHead;
+
+        while(current != null) {
+            System.out.println(current.value);
+            current = current.next;
         }
 
 
         // System.out.println(myNode.value);
         // System.out.println(myNode.next.value);
         // System.out.println(myNode.next.next.value);
+    }
+
+
+    //Count hoe many nodes are in the list
+    public static int length(Node head){
+        int count = 0;
+        Node currentNode = head;
+
+        while(currentNode != null){
+            count++;
+            currentNode = currentNode.next;
+        }
+
+        return count;
+    }
+
+    // Remove node at removeIndex
+    // head is at index 0 
+    public static Node removeAt(Node head, int removeIndex){
+        Node current = head;
+
+        for(int i = 0; i < removeIndex - 1; i++){
+            current = current.next;
+        }
+
+        current.next = current.next.next;
+
+        return head;
     }
 }
